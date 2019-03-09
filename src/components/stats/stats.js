1d410cd042de1a8cd0aedc0cb2c430b3749dd41d
Rynnoceros <jordansoubrier@hotmail.fr>
2019-03-09 03:54:14 +0100

    Working on charts

--- src/components/stats/stats.js
+++ src/components/stats/stats.js
@@ -1,8 +1,17 @@
+const urlInfluDb = "http://localhost:3000/d/OPwpXbCiz/dasboard-test?orgId=1&from=1420142526499&to=1552097509930&var-id=";
+
 export default {
     name : "Stats",
     props: ["item"],
     data() {
         return {
+            source: urlInfluDb
+        }
+    },
+    watch: {
+        item: function(newVal, oldVal) {
+            console.log("item changed");
+            this.source = urlInfluDb + this.item;
         }
     }
 }
