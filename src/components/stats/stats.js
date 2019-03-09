8bc55bfd5f89ba61980cf6c54ba029e96c90d854
Rynnoceros <jordansoubrier@hotmail.fr>
2019-03-09 15:27:57 +0100

    Finishing project

--- src/components/stats/stats.js
+++ src/components/stats/stats.js
@@ -1,17 +1,52 @@
-const urlInfluDb = "http://localhost:3000/d/OPwpXbCiz/dasboard-test?orgId=1&from=1420142526499&to=1552097509930&var-id=";
+import { setInterval } from "timers";
+
+const urlInfluDb = "http://localhost:3000/d/XBeiMfCmk/temperature-evolution?orgId=1&refresh=5s";
+const urlCurrentTemp = "http://localhost:3000/d/eb_As-jmk/current-temperature?orgId=1&refresh=5s";
+const urlInfluxdbWrite = "http://localhost:8086/write?db=smart_use";
 
 export default {
     name : "Stats",
-    props: ["item"],
+    props: ["isLoaded", "item"],
     data() {
         return {
-            source: urlInfluDb
+            source: urlInfluDb,
+            currentTemp: urlCurrentTemp,
+            loaded: false,
+            interval: null
         }
+    },
+    methods: {
+        createRandomData() {
+            var data = "temperature,id="+this.item+" value="+Math.floor(Math.random() * 55)+" "+Math.floor(Date.now())+"000000";
+            fetch(urlInfluxdbWrite, {
+                method: "POST",
+                body: data
+            })
+            .then(response => {
+                console.log("Data sent:"+data);
+            })
+            .catch(error => {
+                console.log("Error generating data:"+error);
+            })
+        },
     },
     watch: {
         item: function(newVal, oldVal) {
             console.log("item changed");
-            this.source = urlInfluDb + this.item;
+            this.source = urlInfluDb + "&var-id=" + this.item;
+            this.currentTemp = urlCurrentTemp + "&var-id=" + this.item;
+            if (this.interval != null) {
+                clearInterval(this.interval);
+                this.interval = null;
+            }
+            if (this.item != null) {
+                if (this.interval == null) {
+                    this.interval = setInterval(this.createRandomData.bind(this),5000);
+                }
+            }
+        },
+        isLoaded: function(newVal, oldVal) {
+            this.loaded = this.isLoaded;
         }
     }
 }
